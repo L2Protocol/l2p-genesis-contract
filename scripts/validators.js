17abee6b29fbadc4a48e40a62d55e26b28258bc7
L2Protocol <github@l2protocol.com>
2025-12-04 11:44:08 +0000

    genesis: new configuration
    Set the new configuration

--- scripts/validators.js
+++ scripts/validators.js
@@ -1,31 +1,31 @@
-const web3 = require('web3');
+const web3 = require('web3')
 const RLP = require('rlp');
 
 // Configure
 const validators = [
-  {
-    consensusAddr: '0xae11fb1f89c83c3ad49636a283732a3692de76f9',
-    feeAddr: '0xae11fb1f89c83c3ad49636a283732a3692de76f9',
-    l2pFeeAddr: '0xae11fb1f89c83c3ad49636a283732a3692de76f9',
-    votingPower: 0x0000000000000064,
-  },
-  {
-    consensusAddr: '0x98803ed812d591b5dcc319652645036b6ca32d1b',
-    feeAddr: '0x98803ed812d591b5dcc319652645036b6ca32d1b',
-    l2pFeeAddr: '0x98803ed812d591b5dcc319652645036b6ca32d1b',
-    votingPower: 0x0000000000000064,
-  },
-  {
-    consensusAddr: '0xda209d1508a1680be75751d0a9923d74997d90f2',
-    feeAddr: '0xda209d1508a1680be75751d0a9923d74997d90f2',
-    l2pFeeAddr: '0xda209d1508a1680be75751d0a9923d74997d90f2',
-    votingPower: 0x0000000000000064,
-  },  
+   {
+     'consensusAddr': '0xaE11fB1F89C83c3AD49636A283732A3692dE76f9',
+     'feeAddr': '0xaE11fB1F89C83c3AD49636A283732A3692dE76f9',
+     'l2pFeeAddr': '0xaE11fB1F89C83c3AD49636A283732A3692dE76f9',
+     'votingPower': 2001,
+   },
+   {
+     'consensusAddr': '0x98803ED812D591B5dcc319652645036B6ca32d1B',
+     'feeAddr': '0x98803ED812D591B5dcc319652645036B6ca32d1B',
+     'l2pFeeAddr': '0x98803ED812D591B5dcc319652645036B6ca32d1B',
+     'votingPower': 2001,
+   },
+   {
+     'consensusAddr': '0xDa209d1508a1680Be75751d0a9923d74997D90F2',
+     'feeAddr': '0xDa209d1508a1680Be75751d0a9923d74997D90F2',
+     'l2pFeeAddr': '0xDa209d1508a1680Be75751d0a9923d74997D90F2',
+     'votingPower': 2001,
+   },
 ];
 const bLSPublicKeys = [
-  '0x804f961443dc8f08658653883fb024d1307b6968433a1b60280e072d85fe247506c983840d55fc8409a9733a7bf94471',
-  '0xa6d7f4c2fc5f961d43fcd6bdaa5224b718641134ab0414f8bd60efa3d82fee41a63d19fb843179961d652644d4451ded',
-  '0xb5d063d021843a1df55d4a17e2a6e8fd4d8002d179533e31a5c33922f9a725f1771c5ab0223509346dbc8fd27e1360e9'
+   '0xb990452e4365ee99b1ae0bef9ade1639c45f9560a7e334abad2b802ae3b6ae53d8a613924e3d94716287438e44aef774',
+   '0x84a27e33f9a4d177ece0792106c648c1b91937782b119e06aa274485798f60bac26b1363656ecf8ecdabade91b292326',
+   '0xab314870c4485be98da76207e4bcbbff0e45506631966e27f9424105351f8a66c44177e1e9f58038878308eee1a3ce77',
 ];
 
 // ======== Do not edit below ========
@@ -33,7 +33,7 @@ function generateExtraData(validators) {
   let extraVanity = Buffer.alloc(32);
   let validatorsBytes = extraDataSerialize(validators);
   let extraSeal = Buffer.alloc(65);
-  return Buffer.concat([extraVanity, validatorsBytes, extraSeal]);
+  return Buffer.concat([extraVanity,validatorsBytes, extraSeal]);
 }
 
 function extraDataSerialize(validators) {
