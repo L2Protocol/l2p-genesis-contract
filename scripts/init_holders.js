1a4462779132f0524660b61364f639d2fa9c8f0f
L2Protocol <github@l2protocol.com>
2026-09-05 12:56:57 +0200

    Set initial holders

--- scripts/init_holders.js
+++ scripts/init_holders.js
@@ -1,8 +1,36 @@
 const web3 = require('web3');
 const init_holders = [
   {
+     address: '0x9dcf2B6c2e4D7B19A0E01f2567a5016eA8d2b64c',
+     balance: BigInt('29000000000000000000000000000').toString(16) // 29000000000e18
+  },
+  {
+     address: '0xfF92dEFc41bf0e35Af7370Af381dE797f4e89133',
+     balance: BigInt('25000000000000000000000000000').toString(16) // 25000000000e18
+  },
+  {
+     address: '0x5Cf2362A30CDc47006Af86A0555e968687C7522d',
+     balance: BigInt('25000000000000000000000000000').toString(16) // 25000000000e18
+  },
+  {
+     address: '0x0894Dd24ECf50eA464E1cEb13443bCcC9684D314',
+     balance: BigInt('15000000000000000000000000000').toString(16) // 15000000000e18
+  },
+  {
+     address: '0xea9277800E88D8fd8292197d2235DDbf8A3Ab1B0',
+     balance: BigInt('7500000000000000000000000000').toString(16) // 7500000000e18
+  },
+  {
+     address: '0xd96092d82E601457b4f215F9F8eabd75AF63b4B6',
+     balance: BigInt('10000000000000000000000000000').toString(16) // 10000000000e18
+  },
+  {
+     address: '0x2f7D41e6E260C8F3c2D9393713032CE2C45Ef1cd',
+     balance: BigInt('3200000000000000000000000000').toString(16) // 3200000000e18
+  },
+  {
      address: '0x63Ae79C826b9FD64cd5D59520987A062240240D3',
-     balance: BigInt('1000000000000000000000000000').toString(16) // 1000000000e18
+     balance: BigInt('21000010000000000000000000').toString(16) // 21000010e18
   },
 ];
 
